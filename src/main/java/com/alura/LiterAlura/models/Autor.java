6cf2ffa3fbd805eadcb7f2168b4aeceeadb21bf0
AndresLeitonDev <andresh.leiton@gmail.com>
2025-01-16 12:32:55 -0500

    :hammer: Construyendo modelos Autor y Libro

---
+++ src/main/java/com/alura/LiterAlura/models/Autor.java
@@ -0,0 +1,59 @@
+package com.alura.LiterAlura.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authors")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "birth_year")
+    private int birthYear;
+
+    @Column(name = "death_year")
+    private int deathYear;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(int deathYear) {
+        this.deathYear = deathYear;
+    }
+}
